390c225d34132cada1d93d11c4f6fd72719c4fcf
Victor Grigoriu <grigoriu@adobe.com>
2019-06-15 16:19:07 +0300

    Formatting

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,10 +1,13 @@
-export const toRna = (dna) => {
-  return dna.split("").map(n => mapping[n]).join("");
+export const toRna = dna => {
+  return dna
+    .split("")
+    .map(n => mapping[n])
+    .join("");
 };
 
 const mapping = {
-  "C": "G",
-  "G": "C",
-  "T": "A",
-  "A": "U"
-}
+  C: "G",
+  G: "C",
+  T: "A",
+  A: "U"
+};
