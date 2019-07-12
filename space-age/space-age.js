6c0f526d728c9499be70ac1e528fc446fcd10451
Victor Grigoriu <grigoriu@adobe.com>
2019-07-12 18:26:51 +0300

    All tests

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,8 +1,14 @@
 const secondsInEarthYear = 31557600;
 
 const orbitalPeriods = {
-  "earth":   1,
-  "mercury": 0.2408467
+  "earth":     1.00000000,
+  "mercury":   0.24084670,
+  "venus":     0.61519726,
+  "mars":      1.88081580,
+  "jupiter":  11.86261500,
+  "saturn":   29.44749800,
+  "uranus":   84.01684600,
+  "neptune": 164.79132000,
 }
 
 export const age = (planet, seconds) => {
