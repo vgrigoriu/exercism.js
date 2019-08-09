b21f04d97632c062061e5a9ae31688d79ba0d9cc
Victor Grigoriu <grigoriu@adobe.com>
2019-08-09 17:23:03 +0300

    You don't need Array.from

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,6 +1,4 @@
 export const isPangram = sentence => {
-  let distinctLetters = new Set(
-    Array.from(sentence.toLowerCase().replace(/[^a-z]/g, ""))
-  );
+  let distinctLetters = new Set(sentence.toLowerCase().replace(/[^a-z]/g, ""));
   return distinctLetters.size === 26;
 };
