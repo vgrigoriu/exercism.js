921b7a43386c7dac7ba9728bcff207f775ef00c8
Victor Grigoriu <grigoriu@adobe.com>
2019-08-10 16:52:04 +0300

    use fill

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -10,10 +10,9 @@ export class Matrix {
   }
 
   get columns() {
-    let transposedMatrix = Array(this.matrix[0].length);
-    for (let row = 0; row < this.matrix[0].length; row++) {
-      transposedMatrix[row] = [];
-    }
+    let transposedMatrix = Array(this.matrix[0].length)
+      .fill(undefined)
+      .map(() => []);
 
     for (let row = 0; row < this.matrix.length; row++) {
       for (let col = 0; col < this.matrix[row].length; col++) {
