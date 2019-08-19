423a88456ebb77a6dfd8dedf1f0b2b70377bbfee
Victor Grigoriu <grigoriu@adobe.com>
2019-08-19 16:49:21 +0300

    test 1

--- bob/bob.js
+++ bob/bob.js
@@ -1,11 +1,3 @@
-/* eslint-disable no-unused-vars */
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const hey = (message) => {
-  //
-  // YOUR CODE GOES HERE
-  //
+  return 'Whatever.'
 };
